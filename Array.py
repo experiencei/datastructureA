04a996e0a196a3f16b8ae34c6169fb820812ed9b
experiencei <ayelojahighbee01@gmail.com>
2023-07-19 16:45:17 +0100

    working on a contain duplicate DSA q with different method and tradeoff with Time Complexity

--- Array.py
+++ Array.py
@@ -31,9 +31,21 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        countS , countT = {} , {}
+        countS, countT = {} , {}
 
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i] ,0)
             countT[s[i]] = 1 + countT.get(t[i] ,0)
+        return countS == countT
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
         return countS == countT
