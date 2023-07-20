8cd6ce2d14d4afa3f3d9c1f34e40a0035e11b723
experiencei <ayelojahighbee01@gmail.com>
2023-07-20 09:44:57 +0100

    solving concatenation of array in DSa by adding the value in array in N times

--- Array.py
+++ Array.py
@@ -54,7 +54,7 @@ class Solution:
         return countS == countT
 
 # Concatenation of an array
-# Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+#QUESTION -->  Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 # Specifically, ans is the concatenation of two nums arrays.
 
 # Solution : is concatenating array is by tutning [ 1 , 3 , 5] to [1 , 3 , 5 , 1 , 3 , 5].
@@ -67,3 +67,15 @@ class Solution:
             for n in nums:
                 ans.append(n)
         return ans
+
+
+# Replace Elements with Greatest Element on Right Side
+# QUESTION --> Given an array arr, replace every element in that array with the greatest element among the elements to its right, 
+# and replace the last element with -1.
+
+# After doing so, return the array.
+
+# Example 1:
+
+# Input: arr = [17,18,5,4,6,1]
+# Output: [18,6,6,6,1,-1]
