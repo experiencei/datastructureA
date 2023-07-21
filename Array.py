45acb3030f14a3e983533339ca5e069a4d47e943
experiencei <ayelojahighbee01@gmail.com>
2023-07-21 12:07:54 +0100

    solving lenght of last words in array

--- Array.py
+++ Array.py
@@ -133,12 +133,15 @@ class Solution:
 #  length of last word
 # we want to return the lenth of the last word after space (" ")
 sample s = " what is my name ? is ibrahim "
+
 class solution:
     def lengthOflastword(self , s : str) -> int:
-        n = 0
-        for i in range(len(s) -1 , -1 , -1):
-            if i != " ":
-                n += 1
-        return n
-                
+        n , length = len(s) - 1 , 0
+        while s[n] == " ":
+            n -= 1
+        while s[n] != " " and n >= 0:
+            length += 1
+            n -= 1
+        return length
+
 
