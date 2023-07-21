ddd60a20495285bcfc94935c45b7efc400800dc4
experiencei <ayelojahighbee01@gmail.com>
2023-07-21 23:38:30 +0100

    working dsa of array question with isSubsequence

--- Array.py
+++ Array.py
@@ -101,6 +101,16 @@ class Solution:
         return arr
 
 # is Subsequence
+# Question Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+# Example 1:
+
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
 # Given two string S and T s=ace and t= amcnme , as long as we can 
 # find string s in correct order in t string.
 #  Solution : --> we have 2 pointer , 1 on string S and other on string T we keep moving the pointer on T as 
