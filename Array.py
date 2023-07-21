c343ed6bbb2edae2dc8cf5a95219e1ec1993817a
experiencei <ayelojahighbee01@gmail.com>
2023-07-21 11:23:39 +0100

    solving Dsa Array isSubsequence using 2 pointer approach

--- Array.py
+++ Array.py
@@ -111,5 +111,13 @@ class Solution:
 class Solution:
     def isSubsequence(self ,  s : str , t : str) -> bool:
         m , n = 0 , 0
-        while m < len(s) and n < 
+        while m < len(s) and n < len(t):
+            if s[m] == t[n]:
+                m += 1
+                n += 1
+            else :
+                n += 1
+        return True if m == len(s) else False
+
+
             
