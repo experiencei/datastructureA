889c2a02608fa9cdb0683b0fb9f46ba29a975a4d
experiencei <ayelojahighbee01@gmail.com>
2023-07-22 09:02:20 +0100

    working on dsa two sum with array

--- Array.py
+++ Array.py
@@ -162,3 +162,6 @@ class solution:
 # Question --> Given an array of integers nums and an integer target,
 #  return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# Solution: [ 2 , 5 , 6 , 4 , 1] target = 9
+    # we can loop through the array by taking one value and adding 
+    # the next value and adjusting the pointer which is basically 0(n2)
