116878b67876a656eb07d67d8fdfad4536f99d8f
experiencei <ayelojahighbee01@gmail.com>
2023-07-22 09:31:19 +0100

    working on dsa two sum with array

--- Array.py
+++ Array.py
@@ -177,3 +177,7 @@ class Solution:
             prevMap[n] = i
         return
 
+# Longest Common Prefix
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+# If there is no common prefix, return an empty string "".
