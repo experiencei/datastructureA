75f4ee83eaf0370dfb8d389ffe904963177e7a3f
experiencei <ayelojahighbee01@gmail.com>
2023-07-25 21:38:22 +0100

    solving dsa of arra Remove element

--- Array.py
+++ Array.py
@@ -246,3 +246,12 @@ class Solution:
 #QUESTION -->  Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
 # The order of the elements may be changed. 
 # Then return the number of elements in nums which are not equal to val.
+class Solution:
+    def removeElement(self, nums : list[int] , val : int) -> int :
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+            return k
+
