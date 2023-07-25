372824d4f3ab3453b2f44eb392c57f73edc4657d
experiencei <ayelojahighbee01@gmail.com>
2023-07-25 21:47:10 +0100

    working on solving dsa of unique email address

--- Array.py
+++ Array.py
@@ -261,3 +261,12 @@ class Solution:
         return k
 
 # Unique Email Addresses
+# QUESTION --> Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+
+# For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
+# If you add periods '.' between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name. 
+# Note that this rule does not apply to domain names.
+
+# For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.
+# If you add a plus '+' in the local name, everything after the first plus sign will be ignored. 
+# This allows certain emails to be filtered. Note that this rule does not apply to domain names.
