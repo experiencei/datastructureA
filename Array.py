9f17cd977fa0e11558fb21581e6223764880154c
experiencei <ayelojahighbee01@gmail.com>
2023-08-01 19:22:53 +0100

    solving dsa Find All Numbers Disappeared in an Array

--- Array.py
+++ Array.py
@@ -457,3 +457,6 @@ class Solution:
         
 
 # Find All Numbers Disappeared in an Array
+
+# Question ==> Given an array nums of n integers where nums[i] is in the range [1, n], 
+#              return an array of all the integers in the range [1, n] that do not appear in nums.
