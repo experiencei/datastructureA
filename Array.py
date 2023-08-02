01e8e75be87bc9397d69c27570cc229c9fc2cc80
experiencei <ayelojahighbee01@gmail.com>
2023-08-02 09:46:46 +0100

    solving dsa maximum number if ballon

--- Array.py
+++ Array.py
@@ -493,3 +493,6 @@ class Solution:
         return res
 
 # Maximum Number of Balloons
+# Question --> Given a string text, you want to use the characters of text to 
+# form as many instances of the word "balloon" as possible.
+# You can use each character in text at most once. Return the maximum number of instances that can be formed
