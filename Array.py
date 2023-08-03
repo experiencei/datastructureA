664bc2612ab083ba52c606f9754f761f71d0f40f
experiencei <ayelojahighbee01@gmail.com>
2023-08-03 07:09:33 +0100

    solving dsa word pattern

--- Array.py
+++ Array.py
@@ -535,3 +535,12 @@ class Solution:
             return False
         charToWord = {}
         wordToChar = {}
+
+        for c, w in zip(pattern, words):
+            if c in charToWord and charToWord[c] != w:
+                return False
+            if w in wordToChar and wordToChar[w] != c:
+                return False
+            charToWord[c] = w
+            wordToChar[w] = c
+        return True
