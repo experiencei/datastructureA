b53d418fc8681b157abce643651def62dfc682fc
experiencei <ayelojahighbee01@gmail.com>
2023-08-03 07:07:15 +0100

    solving dsa word pattern

--- Array.py
+++ Array.py
@@ -518,4 +518,11 @@ class Solution:
         return res
 
 
- 
+#  Word Pattern
+
+# Question --> Given a pattern and a string s, find if s follows the same pattern.
+# Here follow means a full match, such that there is a bijection 
+# between a letter in pattern and a non-empty word in s.
+# Example 1:
+# Input: pattern = "abba", s = "dog cat cat dog"
+# Output: true
