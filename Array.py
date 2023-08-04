d66f0f54567e0368dcae7cf6296105f0260bb479
experiencei <ayelojahighbee01@gmail.com>
2023-08-04 08:08:08 +0100

    solving dsa Top K Frequent Elements

--- Array.py
+++ Array.py
@@ -719,3 +719,17 @@ class Solution:
         count = {}
         freq = [[] for i in range(nums) + 1]
 
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n , c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq)-1 , 0 , -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+            
+
