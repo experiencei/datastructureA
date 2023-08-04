be269af53ccd37434231204c8913bbe82eceb86d
experiencei <ayelojahighbee01@gmail.com>
2023-08-04 08:34:20 +0100

    solving dsa product of Array except self

--- Array.py
+++ Array.py
@@ -752,3 +752,18 @@ class Solution:
                 #    <--------------
                 #   [ 1 , 2 , 3 , 4 ] 
     # output =  [ 1  -> 24 , 1 -> 12 , 2 -> 8 , 6 ]
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
