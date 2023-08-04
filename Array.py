4349ec342ea40f14cd32b8b2b58b2356a6d4be8e
experiencei <ayelojahighbee01@gmail.com>
2023-08-04 07:37:16 +0100

    solving dsa Top K Frequent Elements

--- Array.py
+++ Array.py
@@ -695,3 +695,9 @@ class Solution:
         return mergeSort(nums, 0, len(nums) - 1)
 
 # Top K Frequent Elements
+# Question --> Given an integer array nums and an integer k, return the k most frequent elements. 
+# You may return the answer in any order.
+
+# Example 1:
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
