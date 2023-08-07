b3e89ab6eab9672b49e123e0e93e2d2347d17074
experiencei <ayelojahighbee01@gmail.com>
2023-08-07 13:07:59 +0100

    solving dsa longest consecutive sequence

--- Array.py
+++ Array.py
@@ -852,6 +852,7 @@ class Solution:
         longest = 0
 
         for n in nums:
+            # check if its the start of a sequence
             if (n - 1) not in numsSet:
                 length = 1
                 while( n + 1) in numsSet:
