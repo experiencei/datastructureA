6a9ba1374e6bbe6ef873d4085ae584e634bcca8e
experiencei <ayelojahighbee01@gmail.com>
2023-08-09 14:16:56 +0100

    solving dsa encode and decode tinyurl

--- Array.py
+++ Array.py
@@ -902,23 +902,23 @@ class Solution:
 
 # Encode and Decode TinyURL
 
-Question : Note: This is a companion problem to the System Design problem: Design TinyURL.
-TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and 
-it returns a short URL such as http://tinyurl.com/4e9iAk. Design a class to encode a URL and decode a tiny URL.
+# Question : Note: This is a companion problem to the System Design problem: Design TinyURL.
+# TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and 
+# it returns a short URL such as http://tinyurl.com/4e9iAk. Design a class to encode a URL and decode a tiny URL.
 
-There is no restriction on how your encode/decode algorithm should work.
- You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+# There is no restriction on how your encode/decode algorithm should work.
+#  You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
 
-Implement the Solution class:
+# Implement the Solution class:
 
-Solution() Initializes the object of the system.
-String encode(String longUrl) Returns a tiny URL for the given longUrl.
-String decode(String shortUrl) Returns the original long URL for the given shortUrl. It is guaranteed that the given shortUrl was encoded by the same object.
+# Solution() Initializes the object of the system.
+# String encode(String longUrl) Returns a tiny URL for the given longUrl.
+# String decode(String shortUrl) Returns the original long URL for the given shortUrl. It is guaranteed that the given shortUrl was encoded by the same object.
  
-Example 1:
+# Example 1:
 
-Input: url = "https://leetcode.com/problems/design-tinyurl"
-Output: "https://leetcode.com/problems/design-tinyurl"
+# Input: url = "https://leetcode.com/problems/design-tinyurl"
+# Output: "https://leetcode.com/problems/design-tinyurl"
 
 
 
