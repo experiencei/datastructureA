b1bbce283260ee8d8443ba62852aacf50a9d5ba7
experiencei <ayelojahighbee01@gmail.com>
2023-08-13 23:08:52 +0100

    solving dsa solid wall

--- Array.py
+++ Array.py
@@ -946,9 +946,7 @@ class Codec:
 
 # Solid wall
 
-# Question : There is a rectangular brick wall in front of you with n rows of bricks. 
-# The ith row has some number of bricks each of the same height (i.e., one unit) but they can be of different widths. 
-# The total width of each row is the same.
+# Question : 
 # Draw a vertical line from the top to the bottom and cross the least bricks. 
 # If your line goes through the edge of a brick, then the brick is not considered as crossed.
 #  You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
