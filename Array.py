121967be631a0380f36a4d311bd110e35ec864cb
experiencei <ayelojahighbee01@gmail.com>
2023-08-13 23:09:35 +0100

    solving dsa solid wall

--- Array.py
+++ Array.py
@@ -952,4 +952,4 @@ class Codec:
 # Draw a vertical line from the top to the bottom and cross the least bricks. 
 # If your line goes through the edge of a brick, then the brick is not considered as crossed.
 #  You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
-# Given the 2D array wall that contains the information about the wall, return the minimum number of crossed bricks after drawing such a vertical line.
+Given the 2D array wall that contains the information about the wall, return the minimum number of crossed bricks after drawing such a vertical line.
