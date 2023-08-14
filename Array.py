f526fd3f36d179bda83f0f51de92a0fd4276517f
experiencei <ayelojahighbee01@gmail.com>
2023-08-14 08:56:39 +0100

    solving dsa best time to buy and sell stock

--- Array.py
+++ Array.py
@@ -983,3 +983,9 @@ class Solution:
 # On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
 # However, you can buy it then immediately sell it on the same day.
 # Find and return the maximum profit you can achieve.
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+# Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+# Total profit is 4 + 3 = 7.
