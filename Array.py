a1ca4cb7c056baa28b52374f9f6e6938a02a94ce
experiencei <ayelojahighbee01@gmail.com>
2023-08-15 12:43:16 +0100

    solving dsa subarray sum equals k

--- Array.py
+++ Array.py
@@ -1005,3 +1005,6 @@ class Solution:
 
 
 # Subarray Sum Equals K
+# Question --> 
+# Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+# A subarray is a contiguous non-empty sequence of elements within an array.
