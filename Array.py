f9de3106ee35d1009f4ac86c2da6056f94409af8
experiencei <ayelojahighbee01@gmail.com>
2023-08-17 18:45:39 +0100

    solving dsa subarray sum equals k

--- Array.py
+++ Array.py
@@ -1016,6 +1016,10 @@ class Solution:
 # Solution --> 
 #   sliding window is ineffective because it is 0(n^2)
 #    Efficient : 
+      prefixSum | count
+
+
+result = number of counts of prefixSum gather
 
 class Solution:
     def subarraySum(self, nums: list[int], k: int) -> int:
