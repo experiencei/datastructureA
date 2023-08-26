9b4499d7d664919b348db2f16b073e85b664df69
experiencei <ayelojahighbee01@gmail.com>
2023-08-26 16:50:17 +0100

    solving dsa minimum number of swaps to make string balanced

--- Array.py
+++ Array.py
@@ -1056,4 +1056,19 @@ class Solution:
 
 # Return the minimum number of swaps to make s balanced.
 
-# Solution
+# Solution : 
+
+
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        extraClose, maxClose = 0, 0
+
+        for c in s:
+            if c == "[":
+                extraClose -= 1
+            else:
+                extraClose += 1
+
+            maxClose = max(maxClose, extraClose)
+
+        return (maxClose + 1) // 2  # Or math.ceil(maxClose / 2)
