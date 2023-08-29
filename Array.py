73b3a8507b4ecab683f9a28c153b5de463982c93
experiencei <ayelojahighbee01@gmail.com>
2023-08-29 15:25:28 +0100

    solving dsa Number of Pairs of Interchangeable Rectangle

--- Array.py
+++ Array.py
@@ -1078,7 +1078,7 @@ class Solution:
 
 # Number of Pairs of Interchangeable Rectangle
 Question : 
-You are given n rectangles represented by a 0-indexed 2D integer array rectangles, where rectangles[i] = [widthi, heighti] denotes the width and height of the ith rectangle.
-Two rectangles i and j (i < j) are considered interchangeable if they have the same width-to-height ratio. More formally,
- two rectangles are interchangeable if widthi/heighti == widthj/heightj (using decimal division, not integer division).
-Return the number of pairs of interchangeable rectangles in rectangles.
+# You are given n rectangles represented by a 0-indexed 2D integer array rectangles, where rectangles[i] = [widthi, heighti] denotes the width and height of the ith rectangle.
+# Two rectangles i and j (i < j) are considered interchangeable if they have the same width-to-height ratio. More formally,
+#  two rectangles are interchangeable if widthi/heighti == widthj/heightj (using decimal division, not integer division).
+# Return the number of pairs of interchangeable rectangles in rectangles.
