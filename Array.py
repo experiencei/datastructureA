2ea1e1eb8e41e7b682b2667409883d7ebd7d65ac
experiencei <ayelojahighbee01@gmail.com>
2023-08-30 17:16:55 +0100

    solving dsa maximum products

--- Array.py
+++ Array.py
@@ -1145,3 +1145,11 @@ class Solution:
                     subseq += s[i]
                 if subseq == subseq[::-1]:
                     pali[mask] = len(subseq)
+
+        res = 0
+        for m1 in pali:
+            for m2 in pali:
+                if m1 & m2 == 0: # disjoint
+                    res = max(res , pali[m1] * pali[m2])
+
+        return res
