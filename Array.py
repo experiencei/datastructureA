b2bfa2c9e11a9cd58a1cabf7d7cd9da3042a8404
experiencei <ayelojahighbee01@gmail.com>
2023-09-01 11:52:00 +0100

    solving dsa find All Anagrams in a String

--- Array.py
+++ Array.py
@@ -1208,3 +1208,11 @@ class Solution(object):
 #  You may return the answer in any order.
 # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
 #  typically using all the original letters exactly once.
+
+Example 1:
+
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
