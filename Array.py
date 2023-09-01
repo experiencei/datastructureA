b38f11e08709b79d7d499746ad724d491a816162
experiencei <ayelojahighbee01@gmail.com>
2023-09-01 11:51:25 +0100

    solving dsa find All Anagrams in a String

--- Array.py
+++ Array.py
@@ -1202,4 +1202,9 @@ class Solution(object):
         return result
 
 
-Find All Anagrams in a String
+# Find All Anagrams in a String
+
+Question --> Given two strings s and p, return an array of all the start indices of p's anagrams in s.
+ You may return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ typically using all the original letters exactly once.
