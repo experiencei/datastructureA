09af48bba36a7e18a0011d164402ee7aedb13b2d
experiencei <ayelojahighbee01@gmail.com>
2023-09-04 17:14:46 +0100

    solving dsa largest number

--- Array.py
+++ Array.py
@@ -1283,3 +1283,8 @@ class Solution:
 #   Given a list of non-negative integers nums, 
 #   arrange them such that they form the largest number and return it.
 # Since the result may be very large, so you need to return a string instead of an integer.
+
+Example 1:
+
+Input: nums = [10,2]
+Output: "210"
