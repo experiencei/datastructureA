17069d1a0b31478be0b95616ce8dd14ecd4654d7
experiencei <ayelojahighbee01@gmail.com>
2023-09-05 08:34:56 +0100

    solving dsa continous subarray sum

--- Array.py
+++ Array.py
@@ -1335,3 +1335,23 @@ class Solution:
 # Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
 
 Solution : 
+
+
+#We are basically storing sum%k and storing it in the hashmap and checking it.
+#Math logic is that the overall sum will get cancelled out because of modulo
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        hashmap = {}
+        hashmap[0]=-1
+        summ=0
+        for i,j in enumerate(nums):
+            summ+=j
+            if summ%k in hashmap.keys():
+                if i-hashmap[summ%k]>=2:
+                    return True
+                else:
+                    continue
+            hashmap[summ%k]=i
+        return False
+            
