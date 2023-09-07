2b5d4856f69df2638da450f5439b143bcfd50cc1
experiencei <ayelojahighbee01@gmail.com>
2023-09-07 08:09:29 +0100

    solving dsa repeated dna sequences

--- Array.py
+++ Array.py
@@ -1440,3 +1440,15 @@ class Solution:
 
 # Input: s = "AAAAAAAAAAAAA"
 # Output: ["AAAAAAAAAA"]
+
+
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> list[str]:
+        result = set()
+        previous_sequences = set()
+        for i in range(len(s) - 9):
+            current = s[i:i+10]
+            if current in previous_sequences:
+                result.add(current)
+            previous_sequences.add(current)
+        return list(result)
