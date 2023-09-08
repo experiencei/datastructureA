7b5748521ce8b5949584acfc54d58a23b3a0920b
experiencei <ayelojahighbee01@gmail.com>
2023-09-08 12:21:57 +0100

    solving dsa insert delete getrandom

--- Array.py
+++ Array.py
@@ -1474,3 +1474,43 @@ class Solution:
 # [[], [1], [2], [2], [], [1], [2], []]
 # Output
 # [null, true, false, true, 2, true, false, 2]
+
+from random import choice
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.dict = {}
+        self.list = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.dict:
+            return False
+
+        self.dict[val] = len(self.list)
+        self.list.append(val)
+
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.dict:
+            return False
+
+        idx, last_element = self.dict[val], self.list[-1]
+        self.list[idx], self.dict[last_element] = last_element, idx
+        self.list.pop()
+        del self.dict[val]
+
+        return True
+
+    def getRandom(self) -> int:
+        return choice(self.list)
+
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
