69f4d0359f9999f78f0767bd43461aff481963c8
experiencei <ayelojahighbee01@gmail.com>
2023-09-11 17:37:20 +0100

    solving dsa range sum query 2d immutable

--- Array.py
+++ Array.py
@@ -1545,7 +1545,7 @@ class Solution:
 #  Range Sum Query 2D - Immutable
 
 
-Share
+
 Given a 2D matrix matrix, handle multiple queries of the following type:
 
 Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
@@ -1555,10 +1555,8 @@ NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
 int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
 You must design an algorithm where sumRegion works on O(1) time complexity.
 
- 
 
 Example 1:
-
 
 Input
 ["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
