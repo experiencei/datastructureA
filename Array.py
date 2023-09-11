383fb882afaf80f0eeef7e1877db41293f32643a
experiencei <ayelojahighbee01@gmail.com>
2023-09-11 17:37:41 +0100

    solving dsa range sum query 2d immutable

--- Array.py
+++ Array.py
@@ -1544,26 +1544,23 @@ class Solution:
 
 #  Range Sum Query 2D - Immutable
 
+# Given a 2D matrix matrix, handle multiple queries of the following type:
+
+# Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+# Implement the NumMatrix class:
+
+# NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+# int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+# You must design an algorithm where sumRegion works on O(1) time complexity.
 
 
-Given a 2D matrix matrix, handle multiple queries of the following type:
+# Example 1:
 
-Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
-Implement the NumMatrix class:
-
-NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
-int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
-You must design an algorithm where sumRegion works on O(1) time complexity.
-
-
-Example 1:
-
-Input
-["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
-[[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
-Output
-[null, 8, 11, 12]
-
+# Input
+# ["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
+# [[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
+# Output
+# [null, 8, 11, 12]
 
 
 
