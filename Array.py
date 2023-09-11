b6bb7a942ec276d186a0d584cf21e564dbfe28f5
experiencei <ayelojahighbee01@gmail.com>
2023-09-11 17:36:42 +0100

    solving dsa range sum query 2d immutable

--- Array.py
+++ Array.py
@@ -1537,3 +1537,50 @@ class RandomizedSet:
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
         return len(set(s[i : i + k] for i in range(len(s) - k + 1))) == 2**k
+
+
+
+
+
+#  Range Sum Query 2D - Immutable
+
+Add to List
+
+Share
+Given a 2D matrix matrix, handle multiple queries of the following type:
+
+Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+Implement the NumMatrix class:
+
+NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+You must design an algorithm where sumRegion works on O(1) time complexity.
+
+ 
+
+Example 1:
+
+
+Input
+["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
+[[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
+Output
+[null, 8, 11, 12]
+
+
+
+
+class NumMatrix:
+    def __init__(self, matrix):
+        self.sum_ = [[0] * (len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
+        for i, line in enumerate(matrix):
+            previous = 0
+            for j, num in enumerate(line):
+                previous += num
+                above = self.sum_[i][j + 1]
+                self.sum_[i + 1][j + 1] = previous + above
+
+    def sumRegion(self, row1, col1, row2, col2):
+        sum_col2 = self.sum_[row2 + 1][col2 + 1] - self.sum_[row1][col2 + 1]
+        sum_col1 = self.sum_[row2 + 1][col1] - self.sum_[row1][col1]
+        return sum_col2 - sum_col1
