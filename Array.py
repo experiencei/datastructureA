de04bea298cd27b22df0d876017480538bec4d8e
experiencei <ayelojahighbee01@gmail.com>
2023-09-12 17:42:18 +0100

    solving dsa First Missing Positive

--- Array.py
+++ Array.py
@@ -1626,3 +1626,32 @@ class Solution:
 # Input: nums = [1,2,0]
 # Output: 3
 # Explanation: The numbers in the range [1,2] are all in the array.
+
+# Solution -->
+    class Solution:
+        def firstMissingPositive(self, nums: list[int]) -> int:
+            A = nums
+            for i in range(len(A)):
+                if A[i] < 0:
+                    A[i] = 0
+                
+            for i in range(len(A)):
+                val = abs(A[i])
+                if 1 <= val <= len(A):
+                    if A[val - 1] > 0:
+                        A[val - 1] *= -1
+                    elif A[val - 1] == 0:
+                        A[val - 1] = -1 * (len(A) + 1)
+            
+            for i in range( 1, len(A)+ 1):
+                if A[i -1] >= 0:
+                    return i
+            
+            return len(A) + 1
+            
+        def firstMissingPositive_2(self, nums: list[int]) -> int:
+            new = set(nums)
+            i = 1
+            while i in new:
+                i += 1
+            return i
