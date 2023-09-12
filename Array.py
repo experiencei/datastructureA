9c05947884853ab264bce46589d6c4242ba9bc26
experiencei <ayelojahighbee01@gmail.com>
2023-09-12 11:56:14 +0100

    solving dsa Non-decreasing Array

--- Array.py
+++ Array.py
@@ -1582,4 +1582,5 @@ class NumMatrix:
 
 # Non-decreasing Array
 
-# Question --> 
+# Question --> Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
