a89320490dda0078e9dcbe105687b46f23581af6
experiencei <ayelojahighbee01@gmail.com>
2023-09-12 17:41:14 +0100

    solving dsa First Missing Positive

--- Array.py
+++ Array.py
@@ -1620,3 +1620,9 @@ class Solution:
  # First Missing Positive
 #  Question --> Given an unsorted integer array nums, return the smallest missing positive integer.
 # You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+# Example 1:
+
+# Input: nums = [1,2,0]
+# Output: 3
+# Explanation: The numbers in the range [1,2] are all in the array.
