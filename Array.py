2347d2d0c1c43bf250929526cc7e0d792b135eb8
experiencei <ayelojahighbee01@gmail.com>
2023-09-12 12:00:08 +0100

    solving dsa Non-decreasing Array

--- Array.py
+++ Array.py
@@ -1591,3 +1591,20 @@ class NumMatrix:
 # Input: nums = [4,2,3]
 # Output: true
 # Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+
+class Solution:
+    def checkPossibility(self, nums):
+        if len(nums) <= 2:
+            return True
+        changed = False
+        for i, num in enumerate(nums):
+            if i == len(nums) - 1 or num <= nums[i + 1]:
+                continue
+            if changed:
+                return False
+            if i == 0 or nums[i + 1] >= nums[i - 1]:
+                nums[i] = nums[i + 1]
+            else:
+                nums[i + 1] = nums[i]
+            changed = True
+        return True
