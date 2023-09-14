fb691573bb2d8c8ef5244d38e966f4d62f2a4fea
experiencei <ayelojahighbee01@gmail.com>
2023-09-14 20:42:52 +0100

    solving dsa valid palindrome II

--- Twopointer.py
+++ Twopointer.py
@@ -37,3 +37,9 @@ class Solution:
 
 # Question --> 
 #     Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
