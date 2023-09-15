d55a4f20918d0bc08606b898d376972cb997224c
experiencei <ayelojahighbee01@gmail.com>
2023-09-15 21:10:08 +0100

    solving dsa reverse string

--- Twopointer.py
+++ Twopointer.py
@@ -135,14 +135,14 @@ class Solution:
 
 
 
-# class Solution:
-#     def reverseString(self, s: List[str]) -> None:
-#         """
-#         Do not return anything, modify s in-place instead.
-#         """
-#         l = 0
-#         r = len(s) - 1
-#         while l < r:
-#             s[l],s[r] = s[r],s[l]
-#             l += 1
-#             r -= 1
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            s[l],s[r] = s[r],s[l]
+            l += 1
+            r -= 1
