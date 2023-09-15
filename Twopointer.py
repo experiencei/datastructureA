bd2f15028d4c423d3cf204289c4bbf6e09ee4f68
experiencei <ayelojahighbee01@gmail.com>
2023-09-15 16:52:28 +0100

    solving dsa minimum diff btw highest and lowest of k score

--- Twopointer.py
+++ Twopointer.py
@@ -105,7 +105,7 @@ class Solution:
 
 
 class Solution:
-    def minimumDifference(self, nums: List[int], k: int) -> int:
+    def minimumDifference(self, nums: list[int], k: int) -> int:
         nums.sort()
         l, r = 0, k - 1
         res = float("inf")
