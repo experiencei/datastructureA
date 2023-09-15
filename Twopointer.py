dc9bc0b41ef75e1ffeeec672ef1972d6544ba8bb
experiencei <ayelojahighbee01@gmail.com>
2023-09-15 16:50:32 +0100

    solving dsa minimum diff btw highest and lowest of k score

--- Twopointer.py
+++ Twopointer.py
@@ -88,14 +88,14 @@ class Solution:
 
 
 
- Minimum Difference Between Highest and Lowest of K Scores
+#  Minimum Difference Between Highest and Lowest of K Scores
 
 
-Question --> You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student.
- You are also given an integer k.
-Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
+# Question --> You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student.
+#  You are also given an integer k.
+# Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
 
-Return the minimum possible difference.
+# Return the minimum possible difference.
 
  
 
