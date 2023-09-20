b57b5a90480fa8f135bbaca43a33a8919eb281b9
experiencei <ayelojahighbee01@gmail.com>
2023-09-20 10:45:30 +0100

    solving dsa 3 sum

--- Twopointer.py
+++ Twopointer.py
@@ -322,17 +322,11 @@ class Solution:
 
 
 
-# 167. Two Sum II - Input Array Is Sorted
-# Medium
+#  Two Sum II - Input Array Is Sorted
 
-# 10712
 
-# 1294
 
-# Add to List
-
-# Share
-# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+# Question --> Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
 
 # Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
 
