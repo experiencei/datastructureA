afc2e682089eac2fec81aadfe3430dc28fbf339e
experiencei <ayelojahighbee01@gmail.com>
2023-09-20 10:13:34 +0100

    solving dsa 3 sum

--- Twopointer.py
+++ Twopointer.py
@@ -374,3 +374,6 @@ class Solution:
 
 
 # 3Sum
+
+# Question --> Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+# Notice that the solution set must not contain duplicate triplets.
