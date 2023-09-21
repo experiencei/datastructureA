5ebc97f5a200992fcafbd62c587b082a35152dcb
experiencei <ayelojahighbee01@gmail.com>
2023-09-21 10:36:10 +0100

    solving dsa four sum

--- Twopointer.py
+++ Twopointer.py
@@ -414,3 +414,11 @@ class Solution:
                         l += 1
                         
         return res
+
+# 4Sum
+# Question --> # Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+# 0 <= a, b, c, d < n
+# a, b, c, and d are distinct.
+# nums[a] + nums[b] + nums[c] + nums[d] == target
+# You may return the answer in any order.
+
