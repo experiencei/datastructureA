9f494bbda906b3b39be8bc8457418c55dc84a4b9
experiencei <ayelojahighbee01@gmail.com>
2023-09-22 11:31:59 +0100

    solving dsa two pointer Container With Most Water

--- Twopointer.py
+++ Twopointer.py
@@ -468,3 +468,6 @@ class Solution:
 # Output: 49
 # Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
 #  In this case, the max area of water (blue section) the container can contain is 49.
+
+Solution : we are going to have left and right pointer
+and keep adjusting based on height of the container which is going to give area
