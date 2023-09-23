b63a6fde87a6e131667906d3a46dd63195e6c19d
experiencei <ayelojahighbee01@gmail.com>
2023-09-23 09:59:33 +0100

    solving dsa Number of Subsequences That Satisfy the Given Sum Condition

--- Twopointer.py
+++ Twopointer.py
@@ -494,3 +494,13 @@ class Solution:
 # Question --> You are given an array of integers nums and an integer target.
 # Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target.
 #  Since the answer may be too large, return it modulo 109 + 7.
+
+Example 1:
+
+Input: nums = [3,5,6,7], target = 9
+Output: 4
+Explanation: There are 4 subsequences that satisfy the condition.
+[3] -> Min value + max value <= target (3 + 3 <= 9)
+[3,5] -> (3 + 5 <= 9)
+[3,5,6] -> (3 + 6 <= 9)
+[3,6] -> (3 + 6 <= 9)
