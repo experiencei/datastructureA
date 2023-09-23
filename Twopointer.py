18fa9df6ed2e7f6ff0a84be1ae44a178e35a1fa5
experiencei <ayelojahighbee01@gmail.com>
2023-09-23 10:03:33 +0100

    solving dsa Number of Subsequences That Satisfy the Given Sum Condition

--- Twopointer.py
+++ Twopointer.py
@@ -505,3 +505,21 @@ class Solution:
 # [3,5] -> (3 + 5 <= 9)
 # [3,5,6] -> (3 + 6 <= 9)
 # [3,6] -> (3 + 6 <= 9)
+
+# Time Complexity - O(nlogn)
+# Space Complexity - O(1)
+
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+
+        res, mod = 0, (10**9 + 7)
+
+        left, right = 0, len(nums) - 1
+        while  left <= right:
+            if (nums[left] + nums[right]) > target:
+                right -= 1
+            else:
+                res += 1 << (right - left)
+                left += 1
+        return res % mod
