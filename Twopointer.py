50fd27f140d709aea11b6a9f0e0a1b8508e3a9bc
experiencei <ayelojahighbee01@gmail.com>
2023-09-23 09:58:44 +0100

    solving dsa Number of Subsequences That Satisfy the Given Sum Condition

--- Twopointer.py
+++ Twopointer.py
@@ -490,6 +490,7 @@ class Solution:
 
 
 # Number of Subsequences That Satisfy the Given Sum Condition
-Question --> You are given an array of integers nums and an integer target.
-Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target.
- Since the answer may be too large, return it modulo 109 + 7.
+
+# Question --> You are given an array of integers nums and an integer target.
+# Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target.
+#  Since the answer may be too large, return it modulo 109 + 7.
