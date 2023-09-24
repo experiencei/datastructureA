d44e0d453dfac5220b75037b6e51ab4a4b10f494
experiencei <ayelojahighbee01@gmail.com>
2023-09-24 08:20:40 +0100

    solving dsa Rotate Array

--- Twopointer.py
+++ Twopointer.py
@@ -536,3 +536,24 @@ class Solution:
 # rotate 1 steps to the right: [7,1,2,3,4,5,6]
 # rotate 2 steps to the right: [6,7,1,2,3,4,5]
 # rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+            
+        l, r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+            
+        l, r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
