2b10f59e8d31bb3116bc8db1b2824618a232e632
experiencei <ayelojahighbee01@gmail.com>
2023-09-25 23:32:53 +0100

    solving dsa boat to save people

--- Twopointer.py
+++ Twopointer.py
@@ -577,3 +577,12 @@ class Solution(object):
             right -= 1
             res += 1
         return res
+
+
+
+# Boats to Save People
+# Question --> You are given an array people where people[i] is the weight of the ith person, 
+# and an infinite number of boats where each boat can carry a maximum weight of limit. 
+# Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+# Return the minimum number of boats to carry every given person.
