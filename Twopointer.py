9ab591b5ecfe29e85fa45099f69593351c377e73
experiencei <ayelojahighbee01@gmail.com>
2023-09-27 06:50:18 +0100

    solving dsa trapping rain water

--- Twopointer.py
+++ Twopointer.py
@@ -597,16 +597,9 @@ class Solution(object):
 
 
 
-#         42. Trapping Rain Water
-# Hard
+#  Trapping Rain Water
 
-# 29171
 
-# 418
-
-# Add to List
-
-# Share
 # Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
  
@@ -621,3 +614,23 @@ class Solution(object):
 
 # Input: height = [4,2,0,3,2,5]
 # Output: 9
+
+
+class Solution:
+    def trap(self, height: list[int]) -> int:
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
