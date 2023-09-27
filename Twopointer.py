cd1d5fb93c3015f751002303b0116f47f7b1e26d
experiencei <ayelojahighbee01@gmail.com>
2023-09-27 06:51:13 +0100

    solving dsa trapping rain water

--- Twopointer.py
+++ Twopointer.py
@@ -600,9 +600,8 @@ class Solution(object):
 #  Trapping Rain Water
 
 
-# Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+#Question --> Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
- 
 
 # Example 1:
 
