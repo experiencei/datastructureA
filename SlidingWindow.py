0cd93827ee73af1666ee176a1ac1fb770e4509aa
experiencei <ayelojahighbee01@gmail.com>
2023-09-27 07:10:12 +0100

    solving dsa Contains Duplicate II

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -35,3 +35,17 @@ class Solution:
 
 # Input: nums = [1,2,3,1], k = 3
 # Output: true
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        window = set()
+        L = 0
+
+        for R in range(len(nums)):
+            if R - L > k:
+                window.remove(nums[L])
+                L += 1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+        return False
