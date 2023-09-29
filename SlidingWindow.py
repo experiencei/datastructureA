2d1ae8641fc06d21c332ffabd8eb14d6358c5212
experiencei <ayelojahighbee01@gmail.com>
2023-09-29 20:29:22 +0100

    solving dsa Sub-arrays of Size K

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -59,3 +59,25 @@ class Solution:
                 return True
             window.add(nums[R])
         return False
+
+
+
+1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+Medium
+
+1423
+
+93
+
+Add to List
+
+Share
+Given an array of integers arr and two integers k and threshold, return the number of sub-arrays of size k and average greater than or equal to threshold.
+
+ 
+
+Example 1:
+
+Input: arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+Output: 3
+Explanation: Sub-arrays [2,5,5],[5,5,5] and [5,5,8] have averages 4, 5 and 6 respectively. All other sub-arrays of size 3 have averages less than 4 (the threshold).
