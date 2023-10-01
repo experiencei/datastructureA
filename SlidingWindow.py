14af9806c862f1e4ed091a73db4b71bf395ff169
experiencei <ayelojahighbee01@gmail.com>
2023-10-01 22:56:45 +0100

    solving dsa Longest Substring Without Repeating Characters

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -94,3 +94,27 @@ class Solution:
 # Longest Substring Without Repeating Characters
 
 #Question --> Given a string s, find the length of the longest substring without repeating characters.
+
+
+
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
