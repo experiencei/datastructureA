5bfdfc699b46891c50e53dcd11a7ffcee1fc9f03
experiencei <ayelojahighbee01@gmail.com>
2023-10-04 08:23:39 +0100

    solving dsa longest repeating character replacement

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -120,3 +120,45 @@ class Solution:
             charSet.add(s[r])
             res = max(res, r - l + 1)
         return res
+
+
+Longest Repeating Character Replacement
+Medium
+
+9523
+
+409
+
+Add to List
+
+Share
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+ 
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        
+        l = 0
+        maxf = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+
+            if (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+
+        return (r - l + 1)
