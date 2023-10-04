9eb271dac5cbdd05298e7815d2730ee0f14bc28a
experiencei <ayelojahighbee01@gmail.com>
2023-10-04 08:23:54 +0100

    solving dsa longest repeating character replacement

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -122,16 +122,8 @@ class Solution:
         return res
 
 
-Longest Repeating Character Replacement
-Medium
+# Longest Repeating Character Replacement
 
-9523
-
-409
-
-Add to List
-
-Share
 You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
 
 Return the length of the longest substring containing the same letter you can get after performing the above operations.
