a0711b69cf2354f9f72d3063ae5f6a98e03a9927
experiencei <ayelojahighbee01@gmail.com>
2023-10-05 23:09:46 +0100

    solving dsa permutation in string

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -167,24 +167,24 @@ class Solution:
 
 
 
-567. Permutation in String
-Medium
+# 567. Permutation in String
+# Medium
 
-10705
+# 10705
 
-352
+# 352
 
-Add to List
+# Add to List
 
-Share
-Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+# Share
+# Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
 
-In other words, return true if one of s1's permutations is the substring of s2.
+# In other words, return true if one of s1's permutations is the substring of s2.
 
  
 
-Example 1:
+# Example 1:
 
-Input: s1 = "ab", s2 = "eidbaooo"
-Output: true
-Explanation: s2 contains one permutation of s1 ("ba").
+# Input: s1 = "ab", s2 = "eidbaooo"
+# Output: true
+# Explanation: s2 contains one permutation of s1 ("ba").
