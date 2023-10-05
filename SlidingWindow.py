c953ba30a391e1318108f43a2e57bd68ae19fe3f
experiencei <ayelojahighbee01@gmail.com>
2023-10-05 23:14:33 +0100

    solving dsa permutation in string

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -224,3 +224,62 @@ class Solution:
                 matches -= 1
             l += 1
         return matches == 26
+
+
+
+
+1838. Frequency of the Most Frequent Element
+Medium
+
+2979
+
+81
+
+Add to List
+
+Share
+The frequency of an element is the number of times it occurs in an array.
+
+You are given an integer array nums and an integer k. In one operation, you can choose an index of nums and increment the element at that index by 1.
+
+Return the maximum possible frequency of an element after performing at most k operations.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,4], k = 5
+Output: 3
+Explanation: Increment the first element three times and the second element two times to make nums = [4,4,4].
+4 has a frequency of 3.
+
+
+
+
+
+
+
+
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+        
+        start=0
+        sm=0
+        ans=1
+        
+        for i,val in enumerate(nums):
+            
+            op=val*(i-start)-sm
+            
+            while op>k:
+                op-=val-nums[start]
+                sm-=nums[start]
+                start+=1
+            
+            sm+=val
+            ans=max(ans,i-start+1)
+                    
+        return ans
