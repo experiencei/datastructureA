2e09a317db5baed5ba38df471c543bfba8a5b519
experiencei <ayelojahighbee01@gmail.com>
2023-10-08 11:39:42 +0100

    solving dsa Fruit Into Baskets

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -308,10 +308,10 @@ class Solution:
 #         return ans
 
 
-# Fruit Into Baskets
+#Fruit Into Baskets
 
-# You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
-
+#Question -->  You are visiting a farm that has a single row of fruit trees arranged from left to right. 
+# The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
 # You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
 
 # You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
