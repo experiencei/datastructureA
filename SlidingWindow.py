242d90164c05187d0cdb78af2f2f663f9d9727e2
experiencei <ayelojahighbee01@gmail.com>
2023-10-10 15:36:59 +0100

    solving dsa Find K Closest Elements

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -326,6 +326,8 @@ class Solution:
 # Output: 3
 # Explanation: We can pick from all 3 trees.
 
+# Solution : 
+
 class Solution:
     def totalFruit(self , fruits: list[int]) -> int:
         count = collections.defaultdict(int) #fruitType -> countInBasket
@@ -383,3 +385,49 @@ class Solution:
 
 # Input: arr = [1,2,3,4,5], k = 4, x = 3
 # Output: [1,2,3,4]
+
+# Log(n) + k
+# More code but also more intuitive
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - 1
+
+        # Find index of x or the closest val to x
+        val, idx = arr[0], 0
+        while l <= r:
+            m = (l + r) // 2
+            curDiff, resDiff = abs(arr[m] - x), abs(val - x)
+            if curDiff < resDiff or (curDiff == resDiff and arr[m] < val):
+                val, idx = arr[m], m
+
+            if arr[m] < x:
+                l = m + 1
+            elif arr[m] > x:
+                r = m - 1
+            else:
+                break
+
+        l = r = idx
+        for i in range(k - 1):
+            if l == 0:
+                r += 1
+            elif r == len(arr) - 1 or x - arr[l - 1] <= arr[r + 1] - x:
+                l -= 1
+            else:
+                r += 1
+        return arr[l : r + 1]
+
+
+# Log(n-k) + k
+# Elegant but very difficult to understand
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - k
+
+        while l < r:
+            m = (l + r) // 2
+            if x - arr[m] > arr[m + k] - x:
+                l = m + 1
+            else:
+                r = m
+        return arr[l : l + k]
