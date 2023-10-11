552e141f7a6efb71c6a41ed7b4655bcf125489ab
experiencei <ayelojahighbee01@gmail.com>
2023-10-11 15:33:25 +0100

    solving dsa revisting two pointer

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -334,9 +334,11 @@ class Solution:
         count = collections.defaultdict(int) #fruitType -> countInBasket
         l , total , res = 0 , 0 , 0
         for r in range(len(fruits)):
+            #counting the occurence of the fuit in hashmap 
             count[fruits[r]] += 1
             total += 1
-
+            
+            
             while len(count) > 2:
                 f = fruits[l]
                 count[f] -= 1
