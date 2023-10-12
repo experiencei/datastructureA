56d86bc1fce2f02a19f04a0f816d30576c1dbf9b
experiencei <ayelojahighbee01@gmail.com>
2023-10-12 08:45:04 +0100

    solving dsa Maximum Number of Vowels in a Substring of Given Length

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -446,11 +446,11 @@ class Solution:
 
 # Question --> Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
 
-Example 1:
+# Example 1:
 
-Input: s = "abciiidef", k = 3
-Output: 3
-Explanation: The substring "iii" contains 3 vowel letters.
+# Input: s = "abciiidef", k = 3
+# Output: 3
+# Explanation: The substring "iii" contains 3 vowel letters.
 
 
 Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
