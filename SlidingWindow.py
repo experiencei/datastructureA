307adb0985994c7db551ed928b727a2dbeca34f1
experiencei <ayelojahighbee01@gmail.com>
2023-10-16 09:54:44 +0100

    solving dsa Minimum Size Subarray Sum

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -476,3 +476,6 @@ class Solution:
 
 
 # Minimum Size Subarray Sum
+
+# Question --> Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. 
+# If there is no such subarray, return 0 instead.
