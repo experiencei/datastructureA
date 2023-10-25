d78d75372857c5d835cf0c10f3e7aae03f5302a7
experiencei <ayelojahighbee01@gmail.com>
2023-10-25 11:12:07 +0100

    solving dsa valid parentheses

--- Stack.py
+++ Stack.py
@@ -11,3 +11,18 @@
 # Solution : we can only have as many as open bracket to begin with {{[()]}} we can start with close )]}}{{[(
 #  we will be having a hashmap to check wether the closing match the key to know the opening before popping from stack
 # Map = {")": "(", "]": "[", "}": "{"} to check the key to the closing bracket in stack
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
