d2e3c3c091960ba990584c220bda302db124f79a
experiencei <ayelojahighbee01@gmail.com>
2023-10-25 11:35:19 +0100

    solving dsa Baseball Game

--- Stack.py
+++ Stack.py
@@ -51,3 +51,15 @@ class Solution:
 # Return the sum of all the scores on the record after applying all the operations.
 
 # The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
+
+# Example 1:
+
+# Input: ops = ["5","2","C","D","+"]
+# Output: 30
+# Explanation:
+# "5" - Add 5 to the record, record is now [5].
+# "2" - Add 2 to the record, record is now [5, 2].
+# "C" - Invalidate and remove the previous score, record is now [5].
+# "D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+# "+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+# The total sum is 5 + 10 + 15 = 30.
