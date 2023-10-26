bfafd39d2980b9bf5beb741e3ff333d8e82721e6
experiencei <ayelojahighbee01@gmail.com>
2023-10-26 08:14:45 +0100

    solving dsa Implement Stack using Queues

--- Stack.py
+++ Stack.py
@@ -99,3 +99,20 @@ class Solution:
 # int pop() Removes the element on the top of the stack and returns it.
 # int top() Returns the element on the top of the stack.
 # boolean empty() Returns true if the stack is empty, false otherwise.
+
+
+# Example 1:
+
+# Input
+# ["MyStack", "push", "push", "top", "pop", "empty"]
+# [[], [1], [2], [], [], []]
+# Output
+# [null, null, null, 2, 2, false]
+
+# Explanation
+# MyStack myStack = new MyStack();
+# myStack.push(1);
+# myStack.push(2);
+# myStack.top(); // return 2
+# myStack.pop(); // return 2
+# myStack.empty(); // return False
