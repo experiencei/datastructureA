647b6fff6fd87283da0e2cf89bea9e762734f5e8
experiencei <ayelojahighbee01@gmail.com>
2023-10-27 07:17:56 +0100

    solving dsa Evaluate Reverse Polish Notation

--- Stack.py
+++ Stack.py
@@ -222,3 +222,23 @@ class MinStack:
 # Input: tokens = ["2","1","+","3","*"]
 # Output: 9
 # Explanation: ((2 + 1) * 3) = 9
+
+
+# Solution --> 
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
