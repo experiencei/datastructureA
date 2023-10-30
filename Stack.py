5599cbdcf7f276342437222f75f440f0f64d27ec
experiencei <ayelojahighbee01@gmail.com>
2023-10-30 17:03:26 +0100

    solving dsa Generate Parentheses

--- Stack.py
+++ Stack.py
@@ -250,3 +250,7 @@ class Solution:
 
 # Generate Parentheses
 # Question --> Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+# Example 1:
+
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
