f618e3f79853e9205ac19799031cb4cce6a2b916
experiencei <ayelojahighbee01@gmail.com>
2023-11-01 06:41:24 +0100

    solving dsa Validate Stack Sequences

--- Stack.py
+++ Stack.py
@@ -334,5 +334,5 @@ class Solution:
 
 # Validate Stack Sequences
 
-Question --> Given two integer arrays pushed and popped each with distinct values, 
-    return true if this could have been the result of a sequence of push and pop operations on an initially empty stack, or false otherwise.
+# Question --> Given two integer arrays pushed and popped each with distinct values, 
+#     return true if this could have been the result of a sequence of push and pop operations on an initially empty stack, or false otherwise.
