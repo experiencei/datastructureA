646b637885ec9a6726019ab3ce0a05e356e6443e
experiencei <ayelojahighbee01@gmail.com>
2023-11-02 08:16:33 +0100

    solving dsa asteroid collision

--- Stack.py
+++ Stack.py
@@ -363,3 +363,13 @@ class Solution:
                 i += 1
 
         return not stack
+
+
+# Asteroid Collision
+
+# Question --> We are given an array asteroids of integers representing asteroids in a row.
+# For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left).
+# Each asteroid moves at the same speed.
+
+# Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode.
+# If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
