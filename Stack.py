1826c3fa84a998a9fb1631d7eda9dc1cbddf4c92
experiencei <ayelojahighbee01@gmail.com>
2023-11-03 08:19:22 +0100

    solving dsa  Daily Temperatures

--- Stack.py
+++ Stack.py
@@ -419,3 +419,17 @@ class Solution:
 
 # Input: temperatures = [73,74,75,71,69,72,76,73]
 # Output: [1,1,4,2,1,1,0,0]
+
+# Solution --> we will be getting the index and temperature
+
+class Solution:
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res
