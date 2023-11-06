65a6512d64591f9d8b590eb4abcc50bf5e581ed2
experiencei <ayelojahighbee01@gmail.com>
2023-11-06 07:38:13 +0100

    solving dsa online stock span

--- SlidingWindow.py
+++ SlidingWindow.py
@@ -470,8 +470,11 @@ class Solution:
         l , cnt , res =  0 , 0 , 0
 
         for r in range(len(s)):
-
             cnt += 1 if s[r] in lib else 0
+            
+            if  r - l + 1 > k:
+                cnt += 1 if s[r] in lib else 0
+                l += 1
             res = max(cnt , res)
         return res
 
