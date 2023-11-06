ec93cdbc4becc0aa2667358fda4496c666ffdfb7
experiencei <ayelojahighbee01@gmail.com>
2023-11-06 07:10:58 +0100

    solving dsa online stock span

--- Stack.py
+++ Stack.py
@@ -481,7 +481,9 @@ class Solution:
 # stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
 # stockSpanner.next(85);  // return 6
 
-
+solution --> 
+#  we will be using monotic decreasing stack for efficiency
+for stack created we want to have (price and span)
 class StockSpanner:
     def __init__(self):
         self.stack = []  # pair: (price, span)
