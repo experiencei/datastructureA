7c90d840c3265a11b91f29348963362fa5a88729
experiencei <ayelojahighbee01@gmail.com>
2023-11-07 06:49:52 +0100

    solving dsa car fleet

--- Stack.py
+++ Stack.py
@@ -522,3 +522,12 @@ class StockSpanner:
 # If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
 
 # Return the number of car fleets that will arrive at the destination.
+
+
+Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+Output: 3
+Explanation:
+The cars starting at 10 (speed 2) and 8 (speed 4) become a fleet, meeting each other at 12.
+The car starting at 0 does not catch up to any other car, so it is a fleet by itself.
+The cars starting at 5 (speed 1) and 3 (speed 3) become a fleet, meeting each other at 6. The fleet moves at speed 1 until it reaches target.
+Note that no other cars meet these fleets before the destination, so the answer is 3.
