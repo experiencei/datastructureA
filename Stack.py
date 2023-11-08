030864ca7f557a2a6649ea6bf2074852b30862ad
experiencei <ayelojahighbee01@gmail.com>
2023-11-08 07:02:13 +0100

    solving dsa Simplify Path

--- Stack.py
+++ Stack.py
@@ -605,3 +605,24 @@ class Solution:
 # Input: path = "/home/"
 # Output: "/home"
 # Explanation: Note that there is no trailing slash after the last directory name.
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        stack = []
+
+        for i in path.split("/"):
+            #  if i == "/" or i == '//', it becomes '' (empty string)
+
+            if i == "..":
+                if stack:
+                    stack.pop()
+            elif i == "." or i == '':
+                # skip "." or an empty string
+                continue
+            else:
+                stack.append(i)
+
+        res = "/" + "/".join(stack)
+        return res
