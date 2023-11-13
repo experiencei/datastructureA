89495f8b420060dad048418c89f2f2a3a18638b1
experiencei <ayelojahighbee01@gmail.com>
2023-11-13 07:33:32 +0100

    solving dsa Remove All Adjacent Duplicates in String II

--- Stack.py
+++ Stack.py
@@ -713,3 +713,9 @@ class Solution:
 # causing the left and the right side of the deleted substring to concatenate together.
 # We repeatedly make k duplicate removals on s until we no longer can.
 # Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
