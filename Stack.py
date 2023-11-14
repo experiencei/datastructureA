db25d475475cc4cde432f873db66b5305dac09c5
experiencei <ayelojahighbee01@gmail.com>
2023-11-14 07:14:24 +0100

    solving dsa Remove K Digits

--- Stack.py
+++ Stack.py
@@ -766,3 +766,6 @@ class Solution:
 # Input: num = "1432219", k = 3
 # Output: "1219"
 # Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+
+
+Solution --> 
