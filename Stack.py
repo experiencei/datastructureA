d9a50a29e73f2f56b4534ff3173a54a910e1fe54
experiencei <ayelojahighbee01@gmail.com>
2023-11-14 07:25:34 +0100

    solving dsa Remove K Digits

--- Stack.py
+++ Stack.py
@@ -769,4 +769,5 @@ class Solution:
 
 
 Solution --> i) we want to probably remove the larger digit and ii) we're biased towards left i.e ( removing the larger digit )
-iii) if we're in decresing order 54321 we want to remove from the left
+iii) if we're in decresing order 54321 we want to remove from the left iii) if we're in increasing order 12345 we want to remove the larger value from the right
+MONOTONIC INCREASING ORDER algorithm 
