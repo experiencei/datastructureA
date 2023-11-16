a602eb436c45aea361439658be167e02b1fd05d3
experiencei <ayelojahighbee01@gmail.com>
2023-11-16 08:11:37 +0100

    solving dsa Maximum Frequency Stack

--- Stack.py
+++ Stack.py
@@ -864,3 +864,12 @@ class Solution:
 # void push(int val) pushes an integer val onto the top of the stack.
 # int pop() removes and returns the most frequent element in the stack.
 # If there is a tie for the most frequent element, the element closest to the stack's top is removed and returned.
+
+
+# Example 1:
+
+# Input
+# ["FreqStack", "push", "push", "push", "push", "push", "push", "pop", "pop", "pop", "pop"]
+# [[], [5], [7], [5], [7], [4], [5], [], [], [], []]
+# Output
+# [null, null, null, null, null, null, null, 5, 7, 5, 4]
