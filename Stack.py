0e8f8a39711cb09107fb3dfcc6b8c6555dc020eb
experiencei <ayelojahighbee01@gmail.com>
2023-11-16 08:10:59 +0100

    solving dsa Maximum Frequency Stack

--- Stack.py
+++ Stack.py
@@ -856,5 +856,11 @@ class Solution:
 
 # Maximum Frequency Stack
 
+Question--> Design a stack-like data structure to push elements to the stack 
+and pop the most frequent element from the stack.
+Implement the FreqStack class:
 
-Question--> Design a stack-like data structure to push elements to the stack and pop the most frequent element from the stack.
+FreqStack() constructs an empty frequency stack.
+void push(int val) pushes an integer val onto the top of the stack.
+int pop() removes and returns the most frequent element in the stack.
+If there is a tie for the most frequent element, the element closest to the stack's top is removed and returned.
