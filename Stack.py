78560cc8dd270ca27f0ffd0830fad5de4443095e
experiencei <ayelojahighbee01@gmail.com>
2023-11-16 08:14:29 +0100

    solving dsa Maximum Frequency Stack

--- Stack.py
+++ Stack.py
@@ -887,3 +887,6 @@ class Solution:
 # freqStack.pop();   // return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
 # freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,4].
 # freqStack.pop();   // return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
+
+
+Solution ---> 
