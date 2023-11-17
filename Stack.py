26592db68de883d74ab0b3d98d5065955bc17733
experiencei <ayelojahighbee01@gmail.com>
2023-11-17 13:41:57 +0100

    solving dsa Largest Rectangle in Histogram

--- Stack.py
+++ Stack.py
@@ -979,3 +979,26 @@ class FreqStack:
 # Output: 10
 # Explanation: The above is a histogram where width of each bar is 1.
 # The largest rectangle is shown in the red area, which has an area = 10 units
+
+
+
+
+Solution --> Solution
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        maxArea = 0
+        stack = []  # pair: (index, height)
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                maxArea = max(maxArea, height * (i - index))
+                start = index
+            stack.append((start, h))
+
+        for i, h in stack:
+            maxArea = max(maxArea, h * (len(heights) - i))
+        return maxArea
