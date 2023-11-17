d7cf9bf86378cacade2c4476d4e7e62cb88c1f37
experiencei <ayelojahighbee01@gmail.com>
2023-11-17 13:11:46 +0100

    solving dsa Largest Rectangle in Histogram

--- Stack.py
+++ Stack.py
@@ -935,9 +935,37 @@ class FreqStack:
 
         # reduce the count of the res from stack
         self.cnt[res] -= 1
-        
+
         # if after poppping from the stack it happens to be empty then we want to reduce the count of the maxCount
         if not self.stack[self.maxCnt]:
             self.maxCnt -= 1
         return res
         
+
+
+# class FreqStack:
+
+#     def __init__(self):
+#         self.freq = defaultdict(int)
+#         self.groups = defaultdict(list)
+#         self.maxFreq = 0
+        
+
+#     def push(self, val: int) -> None:
+#         self.freq[val] += 1
+
+#         if self.freq[val] > self.maxFreq: self.maxFreq = self.freq[val]
+
+#         self.groups[self.freq[val]].append(val)
+
+#     def pop(self) -> int:
+#         first = self.groups[self.maxFreq].pop()
+#         self.freq[first] -= 1
+
+#         if not self.groups[self.maxFreq]:
+#             self.maxFreq -= 1
+
+#         return first
+
+
+# Largest Rectangle in Histogram
