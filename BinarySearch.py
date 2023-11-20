a2a25c52a7da6b330ed6fd811abe569ef212540d
experiencei <ayelojahighbee01@gmail.com>
2023-11-20 07:53:27 +0100

    solving dsa binary search

--- BinarySearch.py
+++ BinarySearch.py
@@ -22,4 +22,7 @@ class Solution:
     while l <= r:
       m = (l + r) // 2
       if nums[m] > target:
-        
+        r = m - 1
+      elif nums[m] < target:
+        l = m + 1
+      else : return m
