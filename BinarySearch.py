7df891272a12319fbdd9f13cefcde2732ec7bce3
experiencei <ayelojahighbee01@gmail.com>
2023-11-20 07:56:34 +0100

    solving dsa binary search

--- BinarySearch.py
+++ BinarySearch.py
@@ -20,7 +20,7 @@ class Solution:
     l , r = 0 , len(nums) - 1
 
     while l <= r:
-      m = (l + r) // 2
+      m = l + ((r - l) // 2) 
       if nums[m] > target:
         r = m - 1
       elif nums[m] < target:
