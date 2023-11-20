50ca26ef4f6b190a9dd2afd1ae7e05a379148712
experiencei <ayelojahighbee01@gmail.com>
2023-11-20 07:45:45 +0100

    solving dsa binary search

--- BinarySearch.py
+++ BinarySearch.py
@@ -4,8 +4,12 @@
 # write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 # You must write an algorithm with O(log n) runtime complexity.
 
+
 # Example 1:
 
 # Input: nums = [-1,0,3,5,9,12], target = 9
 # Output: 4
 # Explanation: 9 exists in nums and its index is 4
+
+solution --> we're going to be using divide and conquer method
+[-1,0,3,5,9,12], since they are already in sorted order
