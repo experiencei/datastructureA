74aa2d7ead449a821d45d174530c9a28bcc2f460
experiencei <ayelojahighbee01@gmail.com>
2023-11-20 07:51:37 +0100

    solving dsa binary search

--- BinarySearch.py
+++ BinarySearch.py
@@ -18,4 +18,8 @@
 class Solution:
   def binarySearch(self, nums : list[int] , target : int) -> int:
     l , r = 0 , len(nums) - 1
-    
+
+    while l <= r:
+      m = (l + r) // 2
+      if nums[m] > target:
+        
