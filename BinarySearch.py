e31ce285334c10d8c30a18384ee59bce56618054
experiencei <ayelojahighbee01@gmail.com>
2023-11-22 08:00:58 +0100

    solving dsa search insert position

--- BinarySearch.py
+++ BinarySearch.py
@@ -60,10 +60,16 @@ class Solution:
 # return the index where it would be if it were inserted in order
  
 class Solution:
-  def searchInsert(self, nums : list[int] , target : int) -> int :
-    l , r = 0 , len(nums)
-    while l < r :
-      mid = l + (r - l) // 2
-      
+    def searchInsert(self, nums: List[int], target: int) -> int:
         # O(log n) and O(1)
+        
+        
+        low, high = 0, len(nums)
+        while low<high:
+            mid = low +(high - low) // 2
+            if target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
 
