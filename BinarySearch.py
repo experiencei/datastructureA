11f206e265b8af597c09bc632c2b98d00015c23b
experiencei <ayelojahighbee01@gmail.com>
2023-11-23 17:36:24 +0100

    solving dsa Guess Number Higher or Lower

--- BinarySearch.py
+++ BinarySearch.py
@@ -87,3 +87,7 @@ class Solution:
 
 
 # Guess Number Higher or Lower
+
+# Question --> We are playing the Guess Game. The game is as follows:
+# I pick a number from 1 to n. You have to guess which number I picked.
+# Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
