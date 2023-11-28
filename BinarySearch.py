e058856260c316297b526d6c31ef34039674656b
experiencei <ayelojahighbee01@gmail.com>
2023-11-28 15:41:58 +0100

    solving dsa arranging coins

--- BinarySearch.py
+++ BinarySearch.py
@@ -132,3 +132,8 @@ class Solution:
 
 
 # Arranging Coins
+
+Question --> You have n coins and you want to build a staircase with these coins. 
+The staircase consists of k rows where the ith row has exactly i coins. 
+The last row of the staircase may be incomplete.
+Given the integer n, return the number of complete rows of the staircase you will build.
