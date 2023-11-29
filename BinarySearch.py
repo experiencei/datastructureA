0f16d7150c4809dbe4f02e71841936ea3f4d334a
experiencei <ayelojahighbee01@gmail.com>
2023-11-29 07:24:50 +0100

    solving dsa Squares of a Sorted Array

--- BinarySearch.py
+++ BinarySearch.py
@@ -203,3 +203,7 @@ class Solution:
 # Output: [0,1,9,16,100]
 # Explanation: After squaring, the array becomes [16,1,0,9,100].
 # After sorting, it becomes [0,1,9,16,100].
+
+
+
+Solution --> we will be using binary search on the array to get efficient algorithm 0(n)
