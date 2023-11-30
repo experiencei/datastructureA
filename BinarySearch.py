0e91c2e004139f81ecfc6b587000200105ca7ffe
experiencei <ayelojahighbee01@gmail.com>
2023-11-30 15:14:15 +0100

    solving dsa Valid Perfect Square

--- BinarySearch.py
+++ BinarySearch.py
@@ -249,5 +249,18 @@ class Solution:
 # Output: true
 # Explanation: We return true because 4 * 4 = 16 and 4 is an integer
 
-       
+
+class Solution:
+     def isPerfectSquare(self, num: int) -> bool:
+        # initializing the left and right pointer
+        l ,r = 1, num
+        while l <= r:
+            mid = (l +r) // 2
+            if mid * mid > num:
+                r = mid - 1
+            elif mid * mid < num:
+                l = mid + 1
+            else:
+                return True
+        return False
            
