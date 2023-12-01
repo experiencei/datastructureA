929cc4bd07098cf663fa5332877212350e351de8
experiencei <ayelojahighbee01@gmail.com>
2023-12-01 19:02:17 +0100

    solving dsa Sqrt(x)

--- BinarySearch.py
+++ BinarySearch.py
@@ -274,4 +274,7 @@ class Solution:
             
         # return false if it exist the while loops without returning true
         return False
-           
+
+
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+The returned integer should be non-negative as well.
