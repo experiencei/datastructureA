26a62df9a0ab0f665f525b928727cb85ba8f59d3
experiencei <ayelojahighbee01@gmail.com>
2023-12-01 19:05:13 +0100

    solving dsa Sqrt(x)

--- BinarySearch.py
+++ BinarySearch.py
@@ -282,3 +282,10 @@ class Solution:
 
 # You must not use any built-in exponent function or operator.
 # For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+
+# Example 1:
+
+# Input: x = 4
+# Output: 2
+# Explanation: The square root of 4 is 2, so we return 2.
