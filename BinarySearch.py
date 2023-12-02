4d4a8d8cfbf2c54ac30ad778391873ce2871d017
experiencei <ayelojahighbee01@gmail.com>
2023-12-02 23:48:12 +0100

    solving dsa Sqrt(x)

--- BinarySearch.py
+++ BinarySearch.py
@@ -289,3 +289,16 @@ class Solution:
 # Input: x = 4
 # Output: 2
 # Explanation: The square root of 4 is 2, so we return 2.
+
+
+class Solution(object):
+    def mySqrt(self, x):
+        l, r = 0, x
+        while l <= r:
+            mid = l + (r-l)//2
+            if mid * mid <= x < (mid+1)*(mid+1):
+                return mid
+            elif x < mid * mid:
+                r = mid - 1
+            else:
+                l = mid + 1
