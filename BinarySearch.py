aac5981731fcee77f5beae413830339b76969ce2
experiencei <ayelojahighbee01@gmail.com>
2023-12-05 11:53:37 +0100

    solving dsa Single Element in a Sorted Array

--- BinarySearch.py
+++ BinarySearch.py
@@ -323,3 +323,19 @@ class Solution(object):
 # Solution ---> we are going to be using binary search on it , to get the single element both side are not going to be the same
 # [1,1,2,3,3,4,4,8,8] like the both side of 2 is totally different and to know the side we need to search for 
 # (since our length of array is always going to be odd) hence we are going to the odd side
+
+
+
+If every element in the sorted array were to appear exactly twice, they would occur in pairs at indices i, i+1 for all even i.
+
+Equivalently, nums[i] = nums[i+1] and nums[i+1] != nums[i+2] for all even i.
+
+When we insert the unique element into this list, the indices of all the pairs following it will be shifted by one, negating the above relationship.
+
+So, for any even index i, we can compare nums[i] to nums[i+1].
+
+If they are equal, the unique element must occur somewhere after index i+1
+If they aren't equal, the unique element must occur somewhere before index i+1
+Using this knowledge, we can use binary search to find the unique element.
+
+We just have to make sure that our pivot index is always even, so we can use mid = 2 * ((lo + hi) // 4) instead of the usual mid = (lo + hi) // 2.
