8155c395d8810ea17e67a0c7859cce180aba9e51
experiencei <ayelojahighbee01@gmail.com>
2023-12-05 09:09:48 +0100

    solving dsa Single Element in a Sorted Array

--- BinarySearch.py
+++ BinarySearch.py
@@ -309,7 +309,7 @@ class Solution(object):
 
 # Single Element in a Sorted Array
 
-Question ---> You are given a sorted array consisting of only integers where every element appears exactly twice,
-except for one element which appears exactly once.
-Return the single element that appears only once.
+# Question ---> You are given a sorted array consisting of only integers where every element appears exactly twice,
+# except for one element which appears exactly once.
+# Return the single element that appears only once.
 # Your solution must run in O(log n) time and O(1) space.
