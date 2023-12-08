7783ef9e59a1edcc79654ed3fa20ae90073ec9d0
experiencei <ayelojahighbee01@gmail.com>
2023-12-08 23:07:25 +0100

    solving dsa Find Peak Element

--- BinarySearch.py
+++ BinarySearch.py
@@ -429,3 +429,16 @@ class Solution:
 
 # Solution ---> it's always guaranted we are going to be having a greater neighbour
 # and we can run our modify binary search on it to get the index of the peak element
+
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        start, end = 0, len(nums) - 1
+        nums.append(float('-inf')) # index -1 and n will both get this in python
+        while start <= end: # binary search to eliminate half each time
+            mid = (start + end) // 2
+            if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
+                return mid
+            if nums[mid - 1] > nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
