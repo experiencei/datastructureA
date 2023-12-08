87d5073a4f6b45f3d82529fca76d2068a82c7c2f
experiencei <ayelojahighbee01@gmail.com>
2023-12-08 22:44:17 +0100

    solving dsa Find Peak Element

--- BinarySearch.py
+++ BinarySearch.py
@@ -419,3 +419,9 @@ class Solution:
 # You may imagine that nums[-1] = nums[n] = -∞. In other words,
 #  an element is always considered to be strictly greater than a neighbor that is outside the array.
 # You must write an algorithm that runs in O(log n) time.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
