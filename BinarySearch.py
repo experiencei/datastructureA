673a702d8dd950d9a2b4bd84b4daaede0f4d536a
experiencei <ayelojahighbee01@gmail.com>
2023-12-13 17:16:50 +0100

    solving dsa Successful Pairs of Spells and Potions

--- BinarySearch.py
+++ BinarySearch.py
@@ -449,6 +449,7 @@ class Solution:
 # Question ---> You are given two positive integer arrays spells and potions, of length n and m respectively, 
 # where spells[i] represents the strength of the ith spell and potions[j] represents the strength of the jth potion.
 
+# You are also given an integer success. A spell and potion pair is considered successful if the product of their strengths is at least success.
 
 
 def successfulPairs(self, spells: list[int], potions: list[int], s: int) -> list[int]:
