a15d5367780bafd6f7237d52d749ac6d82859a73
experiencei <ayelojahighbee01@gmail.com>
2023-12-14 17:36:26 +0100

    solving dsa Search a 2D Matrix

--- BinarySearch.py
+++ BinarySearch.py
@@ -485,10 +485,8 @@ class Solution:
 
 #  Search a 2D Matrix
 
-Question --> You are given an m x n integer matrix matrix with the following two properties:
-
-Each row is sorted in non-decreasing order.
-The first integer of each row is greater than the last integer of the previous row.
-Given an integer target, return true if target is in matrix or false otherwise.
-
-You must write a solution in O(log(m * n)) time complexity.
+# Question --> You are given an m x n integer matrix matrix with the following two properties:
+# Each row is sorted in non-decreasing order.
+# The first integer of each row is greater than the last integer of the previous row.
+# Given an integer target, return true if target is in matrix or false otherwise.
+# You must write a solution in O(log(m * n)) time complexity.
