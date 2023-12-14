d1ef0e92f8677bbf3be02d754f97b3fa4e731eae
experiencei <ayelojahighbee01@gmail.com>
2023-12-14 17:59:18 +0100

    solving dsa Search a 2D Matrix

--- BinarySearch.py
+++ BinarySearch.py
@@ -518,3 +518,17 @@ class Solution:
                 bot = row - 1
             else:
                 break
+
+        if not (top <= bot):
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
