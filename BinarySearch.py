4c6cbf0cb8c9f26e20dca25839eb4350378893fd
experiencei <ayelojahighbee01@gmail.com>
2023-12-14 17:37:11 +0100

    solving dsa Search a 2D Matrix

--- BinarySearch.py
+++ BinarySearch.py
@@ -491,7 +491,7 @@ class Solution:
 # Given an integer target, return true if target is in matrix or false otherwise.
 # You must write a solution in O(log(m * n)) time complexity.
 
-Example 1:
+# Example 1:
 
-Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
-Output: true
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+# Output: true
