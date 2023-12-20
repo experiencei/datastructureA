57dd879b5120bbe02ed091cf7f49dbbda8a1fa32
experiencei <ayelojahighbee01@gmail.com>
2023-12-20 21:02:57 +0100

    solving dsa Search in Rotated Sorted Array

--- BinarySearch.py
+++ BinarySearch.py
@@ -630,3 +630,8 @@ class Solution:
 # For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
 # Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 # You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
