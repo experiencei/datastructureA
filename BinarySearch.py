9714cf7a37c804284ce3e9ad20245785c5c30523
experiencei <ayelojahighbee01@gmail.com>
2023-12-21 07:35:42 +0100

    solving dsa search in rotated sorted array II

--- BinarySearch.py
+++ BinarySearch.py
@@ -668,3 +668,6 @@ class Solution:
 # Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) 
 # such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
 # For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+# Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+# You must decrease the overall operation steps as much as possible.
