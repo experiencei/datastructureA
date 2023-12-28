a68873682d5b05205a592c629eb35cc9c34a7ed4
experiencei <ayelojahighbee01@gmail.com>
2023-12-28 20:59:15 +0100

    solving dsa Find First and Last Position of Element in Sorted Array

--- BinarySearch.py
+++ BinarySearch.py
@@ -780,3 +780,9 @@ class TimeMap:
 
 
 
+# Find First and Last Position of Element in Sorted Array
+
+# Question --> Given an array of integers nums sorted in non-decreasing order,
+# find the starting and ending position of a given target value.
+# If target is not found in the array, return [-1, -1].
+# You must write an algorithm with O(log n) runtime complexity.
