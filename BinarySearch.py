17a24497b45db04165f89746a85586fcf1035f00
experiencei <ayelojahighbee01@gmail.com>
2023-12-29 21:31:59 +0100

    solving dsa Search Suggestions System

--- BinarySearch.py
+++ BinarySearch.py
@@ -832,3 +832,5 @@ class Solution:
 # Design a system that suggests at most three product names from products after each character of searchWord is typed.
 # Suggested products should have common prefix with searchWord.
 # If there are more than three products with a common prefix return the three lexicographically minimums products.
+
+Return a list of lists of the suggested products after each character of searchWord is typed.
