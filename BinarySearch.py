c25b59eb2fa08e8675e2ecaf6cb4e48e8df27290
experiencei <ayelojahighbee01@gmail.com>
2023-12-31 18:02:48 +0100

    solving dsa Split Array Largest Sum

--- BinarySearch.py
+++ BinarySearch.py
@@ -884,3 +884,7 @@ class Solution:
 
 # Question --> Given an integer array nums and an integer k, 
 # split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+
+# Return the minimized largest sum of the split.
+
+# A subarray is a contiguous part of the array.
