1e1784f323bb035017d4d2c8ab952814ffbb225d
experiencei <ayelojahighbee01@gmail.com>
2023-12-31 17:38:48 +0100

    solving dsa search suggestions system

--- BinarySearch.py
+++ BinarySearch.py
@@ -848,5 +848,24 @@ class Solution:
 # Solution --> we want to loop through every words in the products array and use two pointer approach
 # to eliminate those that doesn't have the prefix and return the minimum matching of the search words (3)
 
+class Solution:
+    def suggestedProducts(self, products: list[str] , searchWord: str) -> list[list[str]]:
+        res = []
+        products.sort()
+
+        l , r = 0 , len(products) - 1
+
+        for i in range(len(searchWord)):
+            c = searchWord[i]
+
+            while l <= r and (len(products[l]) <= i or products[l][i] != c):
+                l += 1
+            while l <= r and (len(products[r]) <= i or products[r][i] != c):
+                r -= 1
+
+            res.append([])
+            remain  = r - l + 1
+            for j in range(min(3 , remain)):
+                
 
 
