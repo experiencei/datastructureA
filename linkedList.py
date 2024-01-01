da58c2090ac06d2f01db19d1487ea8b1c076284a
experiencei <ayelojahighbee01@gmail.com>
2024-01-01 07:51:00 +0100

    solving dsa Reverse Linked List

--- linkedList.py
+++ linkedList.py
@@ -7,3 +7,14 @@
 # Output: [5,4,3,2,1]
 
 # Solution --> we want to swing the current with previous value and previous with current value 
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
