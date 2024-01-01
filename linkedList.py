f358078e005ea8df8be5978fc0a3950fd8894b25
experiencei <ayelojahighbee01@gmail.com>
2024-01-01 07:46:51 +0100

    solving dsa Reverse Linked List

--- linkedList.py
+++ linkedList.py
@@ -1,3 +1,7 @@
 # Reverse Linked List
 
-Question --> Given the head of a singly linked list, reverse the list, and return the reversed list.
+# Question --> Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Example 1
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
