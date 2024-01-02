5baddeaaf8289c787294671ae9768e796774582a
experiencei <ayelojahighbee01@gmail.com>
2024-01-02 14:43:58 +0100

    solving dsa Remove Linked List Elements

--- linkedList.py
+++ linkedList.py
@@ -102,5 +102,5 @@ class Solution:
 
 # Remove Linked List Elements
 
-Question --> Given the head of a linked list and an integer val, 
-remove all the nodes of the linked list that has Node.val == val, and return the new head.
+# Question --> Given the head of a linked list and an integer val, 
+# remove all the nodes of the linked list that has Node.val == val, and return the new head.
