4749752574591c343e2b78a3c06204b96565a192
experiencei <ayelojahighbee01@gmail.com>
2024-01-02 14:32:50 +0100

    solving dsa # Palindrome Linked List

--- linkedList.py
+++ linkedList.py
@@ -68,6 +68,33 @@ class Solution:
 # Input: head = [1,2,2,1]
 # Output: true
 
-Solution --> 
+Solution --> we want to loop through all linked list and get the middle of the list with slow pointer
 
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        fast = head
+        slow = head
+        
+        # find the middle (slow)
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+        # reverse second half
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+        
+        # check palindrome
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
 
