1ef08a8f01d5ba0fe1bba9ce61b49c0bee5ae2bd
experiencei <ayelojahighbee01@gmail.com>
2024-01-03 18:08:49 +0100

    solving dsa Remove Duplicates from Sorted List

--- linkedList.py
+++ linkedList.py
@@ -136,6 +136,6 @@ class Solution:
 # Question ---> Given the head of a sorted linked list, delete all duplicates such that each element appears only once. 
 # Return the linked list sorted as well.
 
-Example 1:
-Input: head = [1,1,2]
-Output: [1,2]
+# Example 1:
+# Input: head = [1,1,2]
+# Output: [1,2]
