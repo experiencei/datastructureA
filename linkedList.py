c8b0b159c49f91f3659b06020912304e64a81239
experiencei <ayelojahighbee01@gmail.com>
2024-01-04 15:09:13 +0100

    solving dsa Remove Duplicates from Sorted List

--- linkedList.py
+++ linkedList.py
@@ -160,6 +160,8 @@ class Solution:
 
   # Middle of the Linked List
 
-  Question --> 
+  Question --> Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
 
   
