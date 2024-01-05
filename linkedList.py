5f97f049deb1de5753ea21d1b17aba4e90e43eb2
experiencei <ayelojahighbee01@gmail.com>
2024-01-05 11:00:19 +0100

    solving dsa Intersection of Two Linked Lists

--- linkedList.py
+++ linkedList.py
@@ -191,4 +191,6 @@ class Solution:
 # Intersection of Two Linked Lists
 # Question --> Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect.
 # If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
   
