ee9c0128050e19da4a3fdbc9009410c3d3354538
experiencei <ayelojahighbee01@gmail.com>
2024-01-05 10:59:54 +0100

    solving dsa Intersection of Two Linked Lists

--- linkedList.py
+++ linkedList.py
@@ -189,5 +189,6 @@ class Solution:
         return slow
 
 # Intersection of Two Linked Lists
-Question --> 
+Question --> Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect.
+If the two linked lists have no intersection at all, return null.
   
