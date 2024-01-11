96c31e2a0bd15c19174360eb620a5748f6cbf2e5
experiencei <ayelojahighbee01@gmail.com>
2024-01-11 13:14:32 +0100

    solving dsa remove nth node from end of the list

--- linkedList.py
+++ linkedList.py
@@ -327,4 +327,11 @@ class Solution:
 # Question -->  Given the head of a linked list,
 #  remove the nth node from the end of the list and return its head.
 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
 Solution --> 
