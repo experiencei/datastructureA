b8e41bdea1ab28d820326fe1cd72533f23260e82
experiencei <ayelojahighbee01@gmail.com>
2024-01-11 13:27:48 +0100

    solving dsa Swapping Nodes in a Linked List

--- linkedList.py
+++ linkedList.py
@@ -369,7 +369,8 @@ class Solution:
 # Return the head of the linked list after swapping the values of the kth node from the beginning 
 # and the kth node from the end (the list is 1-indexed).
 
-Example 1:
+# Example 1:
 
-Input: head = [1,2,3,4,5], k = 2
-Output: [1,4,3,2,5]
+# Input: head = [1,2,3,4,5], k = 2
+# Output: [1,4,3,2,5]
+
