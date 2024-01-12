84d87638a1456330bf57fdff647053649cb3b229
experiencei <ayelojahighbee01@gmail.com>
2024-01-12 09:03:01 +0100

    solving dsa Copy List with Random Pointer

--- linkedList.py
+++ linkedList.py
@@ -419,3 +419,20 @@ class Solution:
 
 # Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
 # Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+
+class Solution:
+    def copyRandomList(self, head: "Node") -> "Node":
+        oldToCopy = {None: None}
+
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
+            cur = cur.next
+        return oldToCopy[head]
