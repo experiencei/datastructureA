1996c3b589454666fa9fee89f4f12de46e6bb0d4
experiencei <ayelojahighbee01@gmail.com>
2024-01-14 20:26:25 +0100

    solving dsa design linkedlist

--- linkedList.py
+++ linkedList.py
@@ -443,4 +443,6 @@ class Solution:
 
 Design Linked List
 
-Question --->
+Question ---> Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
