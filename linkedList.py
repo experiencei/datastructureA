7dcaf6804dbf841f12da5cab185e4bd7c30248a4
experiencei <ayelojahighbee01@gmail.com>
2024-01-20 17:02:59 +0100

    solving dsa Design Browser History

--- linkedList.py
+++ linkedList.py
@@ -539,4 +539,6 @@ class MyLinkedList:
 
 # Design Browser History
 
-Question -->
+# Question --> You have a browser of one tab where you start on the homepage and you can visit another url,
+# get back in the history number of steps or move forward in the history number of steps.
+# Implement the BrowserHistory class:
